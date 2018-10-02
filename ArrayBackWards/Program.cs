ab549622d24b4f8db83b97f1b2373afa2c93f484
Fr0thgar <jona270r@edu.easj.dk>
2018-10-02 12:24:14 +0200

    Initial commit

--- ArrayBackWards/Program.cs
+++ ArrayBackWards/Program.cs
@@ -10,6 +10,23 @@ namespace ArrayBackWards
     {
         static void Main(string[] args)
         {
+            int[] number = new int[20];
+            for (int i = number.Length; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            string value =
+                "Never gonna give you up, Never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, Never gonna say goodbye, Never gonna tell a lie and hurt you";
+            
+            char[] array = value.ToCharArray();
+
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                char letter = array[i];
+                Console.Write("Letter: ");
+                Console.WriteLine(letter);
+            }
         }
     }
 }
